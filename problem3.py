fcb37a20b126bc71e25bfade647c2d7954195b3d
Kateryna <g0980509878@gmail.com>
2020-05-28 16:41:50 +0300

    change while to for

---
+++ problem3.py
@@ -0,0 +1,18 @@
+def findLongest(s:str) -> str:
+    """Find the longest substring of s in which letters occur in alphabetical order
+    :return: the longest substring"""
+    longestSubstr = ''
+    orderSubstr = ''
+    prev = ''
+    for i in s:
+        if i >= prev:
+            orderSubstr += i
+            if len(longestSubstr) < len(orderSubstr):
+                longestSubstr = orderSubstr
+        else:
+            orderSubstr = i
+        prev = i
+    return longestSubstr
+
+s = input('Enter your string: ')
+print('Longest substring in alfabetical order is: ', findLongest(s))
